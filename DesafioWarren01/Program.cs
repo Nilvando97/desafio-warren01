701824262a96b50c7cc8ff6cf566a0f958a32c69
Nilvando <nilvandopolicarpo@gmail.com>
2022-05-06 22:10:22 -0300

    Desafio finalizado

---
+++ DesafioWarren01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesafioWarren01
+{
+    class Program
+    {
+        public static int inverter(int n)
+        {
+
+            int resultado = 0;
+
+            while (n > 0)
+            {
+                resultado = 10 * resultado + n % 10;
+                n /= 10;
+            }
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                if((i + inverter(i)) % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
